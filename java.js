71893f4ff0a1c119837f52518ed7cfed2bb9253b
DevShubham-Rao <jimmyrao2010@gmail.com>
2025-05-20 17:35:13 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,10 @@
-
+document.addEventListener("scroll", function () {
+  const images = document.querySelectorAll("img");
+  images.forEach(img => {
+    const position = img.getBoundingClientRect().top;
+    if (position < window.innerHeight - 100) {
+      img.style.opacity = "1";
+      img.style.transform = "translateY(0)";
+    }
+  });
+});
